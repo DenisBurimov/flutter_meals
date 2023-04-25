4f729fb2c9a2450962e005f533c4dfec42dd318d
DenisBurimov <denysburimov@gmail.com>
2023-04-25 20:32:38 +0300

    filters working commit

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -62,11 +62,12 @@ class _TabsScreenState extends State<TabsScreen> {
       activePageTitle = 'Favorites';
     }
 
-    void _setScreen(String identifier) {
+    void _setScreen(String identifier) async {
       Navigator.of(context).pop();
       if (identifier == 'filters') {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
+        final result = await Navigator.of(context).push<Map<Filter, bool>>(
+            MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
+        print(result);
       }
     }
 
