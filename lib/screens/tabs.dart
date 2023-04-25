a809da250ab416a1a1e9b684642b32c07f3d3d7d
DenisBurimov <denysburimov@gmail.com>
2023-04-25 08:56:59 +0300

    working commit

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -61,11 +61,21 @@ class _TabsScreenState extends State<TabsScreen> {
       activePageTitle = 'Favorites';
     }
 
+    void _setScreen(String identifier) {
+      if (identifier == 'filters') {
+        //
+      } else {
+        Navigator.of(context).pop();
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer: const MainDrawer(),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
